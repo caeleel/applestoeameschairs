b313b07fb6a8f0022357981939599e42bc43d365
Karl Jiang <findingthenumber@gmail.com>
2024-10-20 10:41:19 -0400

    fixed pos

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,28 @@ const inter = Inter({ subsets: ['latin'] })
 
 function NavBar() {
   return (
-    <nav className="bg-white relative">
-      <ul className="flex justify-between items-center py-4 px-12 nav-list">
-        <li>
-          <Link href="/battle" className="font-medium nav-item hover:text-primary hover:underline">
-            This vs That
-          </Link>
-        </li>
-        <li>
-          <Link href="/rate" className="font-medium nav-item hover:text-primary hover:underline">
-            1 - 10
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className="font-medium nav-item hover:text-primary hover:underline">
-            Ranking
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="bg-white fixed w-full top-0 z-50">
+        <ul className="flex justify-between items-center py-4 px-12 nav-list">
+          <li>
+            <Link href="/battle" className="font-medium nav-item hover:text-primary hover:underline">
+              This vs That
+            </Link>
+          </li>
+          <li>
+            <Link href="/rate" className="font-medium nav-item hover:text-primary hover:underline">
+              1 - 10
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="font-medium nav-item hover:text-primary hover:underline">
+              Ranking
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex w-full mb-16"></div>
+    </>
   );
 }
 
--- components/Search.tsx
+++ components/Search.tsx
@@ -66,7 +66,7 @@ export default function Search() {
   }
 
   return (
-    <div className="absolute top-12 left-0 right-0">
+    <div className="fixed top-12 left-0 right-0 z-50">
       <div className="bg-white flex items-center justify-start pl-6 pr-10 py-4 h-12 gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
